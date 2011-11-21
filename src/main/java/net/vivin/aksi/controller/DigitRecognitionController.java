8a6d85cc7861f8d82adbb25a5f675ca506cc636f
Vivin Paliath <vivin.paliath@gmail.com>
2011-11-21 09:29:07 -0700

    Tests user input against the neural network and reports the result

--- src/main/java/net/vivin/aksi/controller/DigitRecognitionController.java
+++ src/main/java/net/vivin/aksi/controller/DigitRecognitionController.java
@@ -1,8 +1,14 @@
 package net.vivin.aksi.controller;
 
+import net.vivin.neural.NeuralNetwork;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,7 +22,40 @@ public class DigitRecognitionController {
 
     @RequestMapping
     public void recognize(Model model, String imageData) {
-        model.addAttribute("digit", 9);
-        model.addAttribute("confidence", 0.94494424);
+
+        try {
+            ObjectInputStream in = new ObjectInputStream(new ClassPathResource("DigitRecognizingNeuralNetwork-1321880921458.net").getInputStream());
+            NeuralNetwork neuralNetwork = (NeuralNetwork) in.readObject();
+
+            double[] inputs = new double[imageData.length()];
+            for(int i = 0; i < imageData.length(); i++) {
+                inputs[i] = Double.parseDouble(String.valueOf(imageData.charAt(i)));
+            }
+
+            neuralNetwork.setInputs(inputs);
+
+            double[] output = neuralNetwork.getOutput();
+            double max = output[0];
+            double recognizedDigit = 0;
+            for(int j = 0; j < output.length; j++) {
+                if(output[j] > max) {
+                    max = output[j];
+                    recognizedDigit = j;
+                }
+            }
+
+            model.addAttribute("digit", recognizedDigit);
+            model.addAttribute("confidence", max);
+        }
+
+        catch(IOException e) {
+            model.addAttribute("error", true);
+            model.addAttribute("message", "Unable to load trained network!");
+        }
+
+        catch(ClassNotFoundException e) {
+            model.addAttribute("error", true);
+            model.addAttribute("message", "Unable to load trained network!");
+        }
     }
 }
